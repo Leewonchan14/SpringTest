7a4b75de1b1c859d55cbd6ad4c4837c62103d37f
이원찬 <105588857+Leewonchan14@users.noreply.github.com>
2023-05-13 22:38:34 +0900

    엔티티 설계 완료

---
+++ src/main/java/jpabook/jpashop/OrderItem.java
@@ -0,0 +1,29 @@
+package jpabook.jpashop;
+
+import jpabook.jpashop.item.Item;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+public class OrderItem {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "item_id")
+    private Item item;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    private int orderPrive; //주문 가격
+
+    private int count; //주문 수량
+
+}
